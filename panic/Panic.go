cce19eeb8ffb71399e6ea86ca56450b2cdce2b96
Cory Close <pulsar2612@hotmail.com>
2018-01-26 15:43:53 -0600

    Adding panic package with stack-trace like "IdentifyPanic"

---
+++ panic/Panic.go
@@ -0,0 +1,37 @@
+package panic
+
+import (
+	"runtime"
+	"strings"
+	"fmt"
+)
+
+// Taken from github gist: https://gist.github.com/swdunlop/9629168
+// Pulled into a package so i could reuse
+func IdentifyPanic() string {
+	var name, file string
+	var line int
+	var pc [16]uintptr
+
+	n := runtime.Callers(3, pc[:])
+	for _, pc := range pc[:n] {
+		fn := runtime.FuncForPC(pc)
+		if fn == nil {
+			continue
+		}
+		file, line = fn.FileLine(pc)
+		name = fn.Name()
+		if !strings.HasPrefix(name, "runtime.") {
+			break
+		}
+	}
+
+	switch {
+	case name != "":
+		return fmt.Sprintf("%v:%v", name, line)
+	case file != "":
+		return fmt.Sprintf("%v:%v", file, line)
+	}
+
+	return fmt.Sprintf("pc:%x", pc)
+}
