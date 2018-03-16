51051f29ac582abdf177a2ec557228f5df51928e
Cory Close <cclose@xes-inc.com>
2018-03-16 10:21:01 -0500

    Adding ReturnPanic panic catcher

--- panic/Panic.go
+++ panic/Panic.go
@@ -4,6 +4,7 @@ import (
 	"runtime"
 	"strings"
 	"fmt"
+	"errors"
 )
 
 // Taken from github gist: https://gist.github.com/swdunlop/9629168
@@ -34,4 +35,33 @@ func IdentifyPanic() string {
 	}
 
 	return fmt.Sprintf("pc:%x", pc)
+}
+
+// ReturnPanic
+// @param rErr *error  A pointer to an error variable that will contain the panic
+//
+// ReturnPanic is meant to be deferred in a function that might panic. It will recover
+// the panic into an error, which it will then set into the variable specified in a pointer.
+// This allows your function to handle the panic and return it like a normal err
+//
+// Example:
+// func Foo(i int) (j int, err error) {
+//     defer ReturnPanic(&err)
+//     i2, err := funcThatWontPanic(i)
+//     j := funcThatMightPanic(i2)
+//
+//     return
+// }
+func ReturnPanic(rErr *error) {
+	if r := recover(); r != nil {
+		// Coerce the panic into an error
+		switch x := r.(type) {
+		case string:
+			*rErr = errors.New(x)
+		case error:
+			*rErr = x
+		default:
+			*rErr = errors.New("Unknown panic")
+		}
+	}
 }
